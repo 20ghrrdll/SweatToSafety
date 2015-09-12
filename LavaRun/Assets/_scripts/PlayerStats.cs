4562ad31bcedddeaa9660940edad3bf14c78e86e
Carah Alexander <carah@carahalexander.com>
2015-09-12 15:46:36 -0700

    Had to move web & game into one mega repository

---
+++ LavaRun/Assets/_scripts/PlayerStats.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	int totalNumExercises;
+	public GameObject lava;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//shift lava forwards
+	public void shiftLavaUp(){
+		lava.transform.position += Vector3.left * 40f; 
+	}
+	//shift lava backwards
+	//Jog in place(raise hr)
+	//water break(lower hr)
+	//choose exercise
+	//exercise prompt
+	//exercises 1-whatever
+}
+
+
